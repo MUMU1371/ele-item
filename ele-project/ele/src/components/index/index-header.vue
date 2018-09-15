181e8345d66681e4299bc84c1fc3cfe54ac7ffac
mumu1371 <1015128271@qq.com>
2018-09-15 17:56:58 +0800

    2018.9.15 17：56

---
+++ ele-project/ele/src/components/index/index-header.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "index-header"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ ele-project/ele/src/components/index/index-section.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "index-section"
+    }
+</script>
+
+<style scoped>
+
+</style>
